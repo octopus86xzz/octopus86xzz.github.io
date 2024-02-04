e001f5cf3d9fc9746c20a75429562b494e36a089
86 Octopus <octopus86@163.com>
2024-02-05 01:50:55 +0800

    Site updated: 2024-02-05 01:50:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/02/自我介绍/"];function toRandomPost(){
+var posts=["2024/02/05/青春回归歌曲/","2024/02/02/自我介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
